f79a6ae61750266abb9462f0c814ea3371c19c19
Kanthi Subramanian <subkanthi@gmail.com>
2018-06-09 19:29:59 -0400

    Created proper folder package

--- com/javaRxexamples/observables/src/main/java/OberservableMain.java
+++ com/javaRxexamples/observables/src/main/java/OberservableMain.java
@@ -6,6 +6,8 @@ import io.reactivex.Observable;
 public class OberservableMain {
     public static void main(String[] args)  {
         Observable<String> myStrings =
+                // .just creates an observable from any
+                // type so it can be subscribed.
                 Observable.just("Alpha", "Beta", "Gamma", "Delta",
                         "Epsilon");
 
